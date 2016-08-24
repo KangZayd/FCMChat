8493a8112915b5a1a0bda9b28ee4870764d89933
Iman Tabrizian <Tabrizian@outlook.com>
2016-08-24 14:59:07 +0430

    Improves project structure for firebase

---
+++ app/src/main/java/com/example/iman/fcmchat/MyFirebaseInstanceIDService.java
@@ -0,0 +1,13 @@
+package com.example.iman.fcmchat;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    public MyFirebaseInstanceIDService() {
+    }
+}
