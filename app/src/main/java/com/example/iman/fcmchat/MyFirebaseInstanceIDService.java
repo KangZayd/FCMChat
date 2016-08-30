25f37c154881551f7dd9ce384fe1ce4019a0c022
Iman Tabrizian <Tabrizian@outlook.com>
2016-08-30 14:41:54 +0430

    Getting data from fcm finished

--- app/src/main/java/com/example/iman/fcmchat/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/example/iman/fcmchat/MyFirebaseInstanceIDService.java
@@ -3,11 +3,25 @@ package com.example.iman.fcmchat;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
-    public MyFirebaseInstanceIDService() {
+
+    private static final String TAG = "FirebaseInstance";
+
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+//        sendRegistrationToServer(refreshedToken);
     }
 }
